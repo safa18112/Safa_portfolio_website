77478ed25058199639d2f81f84782826e8bff7b9
safa18112 <itssafashariff18@gmail.com>
2026-04-08 12:57:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ database_url = os.environ.get('DATABASE_URL')
 
 if database_url:
     if database_url.startswith("postgres://"):
-        database_url = database_url.replace("postgres://", "postgresql://", 1)
+         database_url = database_url.replace("postgres://", "postgresql://", 1)
     app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
